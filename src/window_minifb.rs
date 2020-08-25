590c8099372962413e7d5933c9aa215bf1f5ac97
Andreas Neukoetter <andreas@omni-mad.com>
2020-08-25 20:04:01 +0200

    + Added 2x downscaling with multisampling for minifb window.

--- src/window_minifb.rs
+++ src/window_minifb.rs
@@ -5,20 +5,29 @@ use cheval::cheval::Cheval;
 pub struct Window {
 	width: usize,
 	height: usize,
+	downscale: usize, 
 	buffer: Vec<u32>,
+	frame: Vec<u32>,
 	window: minifb::Window,
 }
 
 impl Window {
 	pub fn new() -> Self {
+		let w = 1920;
+		let h = 1080;
+		let ds = 2;
+		let fw = w/ds;
+		let fh = h/ds;
 		let mut s = Self {
-			width: 640,
-			height: 360,
-			buffer: vec![0; 640 * 360], //Vec::new(),
+			width: w,
+			height: h,
+			downscale: ds,
+			buffer: vec![0u32; w * h],
+			frame: vec![0u32; fw * fh],
 			window: minifb::Window::new(
 				"Test",
-				640,
-				360,
+				fw,
+				fh,
 				minifb::WindowOptions::default()
 			).unwrap_or_else(|e| {
         		panic!("{}", e);
@@ -35,8 +44,55 @@ impl Window {
 		func( &mut self.buffer, self.width, self.height, cheval );
 	}
 	pub fn next_frame( &mut self ) {
+		// :TODO: handle multisampling for downscaling
+		// :TODO: actually use downscaling factor for multisampling
+		let ds = self.downscale;
+		let fw = self.width / ds;
+		let fh = self.height / ds;
+
+		for y in 0..fh {
+			for x in 0..fw {
+				let so = ( y * ds ) * self.width + ( x * ds );
+				let mut argb = vec![0u32;4];
+				let pixel = self.buffer[ so ];
+				argb[ 1 ] += ( ( pixel >> 16 ) & 0xff ) as u32;
+				argb[ 2 ] += ( ( pixel >>  8 ) & 0xff ) as u32;
+				argb[ 3 ] += ( ( pixel >>  0 ) & 0xff ) as u32;
+
+				let pixel = self.buffer[ so + 1 ];
+				argb[ 1 ] += ( ( pixel >> 16 ) & 0xff ) as u32;
+				argb[ 2 ] += ( ( pixel >>  8 ) & 0xff ) as u32;
+				argb[ 3 ] += ( ( pixel >>  0 ) & 0xff ) as u32;
+
+				let pixel = self.buffer[ so + self.width ];
+				argb[ 1 ] += ( ( pixel >> 16 ) & 0xff ) as u32;
+				argb[ 2 ] += ( ( pixel >>  8 ) & 0xff ) as u32;
+				argb[ 3 ] += ( ( pixel >>  0 ) & 0xff ) as u32;
+
+				let pixel = self.buffer[ so + self.width + 1 ];
+				argb[ 1 ] += ( ( pixel >> 16 ) & 0xff ) as u32;
+				argb[ 2 ] += ( ( pixel >>  8 ) & 0xff ) as u32;
+				argb[ 3 ] += ( ( pixel >>  0 ) & 0xff ) as u32;
+
+				argb[ 1 ] /= 4;
+				argb[ 2 ] /= 4;
+				argb[ 3 ] /= 4;
+
+
+				let pixel = 
+					( ( argb[ 1 ] & 0xff ) << 16 )
+					| ( ( argb[ 2 ] & 0xff ) <<  8 )
+					| ( ( argb[ 3 ] & 0xff ) <<  0 );
+
+//				let pixel = pixel + self.buffer[ so + 1 ];
+//				let pixel = pixel / 2;
+				let fo = y * fw + x;
+//				if y >= 270 { dbg!(&x, &fo); }
+				self.frame[ fo ] = pixel;
+			}
+		}
         self.window
-            .update_with_buffer(&self.buffer, self.width, self.height )
+            .update_with_buffer(&self.frame, fw, fh )
             .unwrap();
 	}
 }
