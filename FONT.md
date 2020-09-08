51e7176402ac51210d267b3eb67981a3ff8c14d0
Andreas Neukoetter <andreas@omni-mad.com>
2020-09-08 18:51:00 +0200

    + Add command line handling.

---
+++ FONT.md
@@ -0,0 +1,1 @@
+https://github.com/tonsky/FiraCode/releases/download/5.2/Fira_Code_v5.2.zip
--- src/cheval_main.rs
+++ src/cheval_main.rs
@@ -3,6 +3,7 @@
 use cheval::cheval::Cheval;
 use cheval::block_element::{BlockElement,BlockElementFactory};
 use cheval::element::{Element,ElementConfig};
+use clap::{App, Arg};
 
 fn render_frame( buffer: &mut Vec<u32>, width: usize, height: usize, cheval: &Cheval )
 {
@@ -19,11 +20,28 @@ fn render_frame( buffer: &mut Vec<u32>, width: usize, height: usize, cheval: &Ch
 #[tokio::main]
 async fn main() -> Result<(),Box<dyn std::error::Error>> {
 
+	let matches = App::new("cheval")
+						.version("0.1")
+						.author("Andreas N. <andreas@omni-mad.com>")
+						.arg( Arg::with_name("config")
+							.long("config")
+							.short("c")
+							.value_name("CONFIG")
+							.help("Set the config file to load.")
+							.takes_value(true)
+						)
+						.get_matches();
+
+	let config = matches.value_of("config").unwrap_or("example_config.yaml").to_string();
+
+	dbg!(&config);
+
+
 	let mut window = Window::new();
 
 	let mut cheval = Cheval::new();
 
-	cheval.load( "example_config.yaml" ).await?;
+	cheval.load( &config ).await?;
 
 	dbg!( &cheval );
 	while !window.done() {
