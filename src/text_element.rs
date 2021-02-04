3ab95ddb876324568a0ee397c36c357f465e3e0d
Andreas Neukoetter <andreas@omni-mad.com>
2021-02-04 19:40:45 +0100

    - Remove old code.

--- src/text_element.rs
+++ src/text_element.rs
@@ -71,42 +71,6 @@ impl Element for TextElement {
 
 		self.bounding_box = bb;
 
-		// load font
-		/*
-let mut f = match File::open(input[ 0 ]) {
-			Ok( f ) => f,
-			Err( _ ) => return Err(OmError::Generic("io".to_string())),
-		};
-
-		let mut buffer = Vec::new();
-
-	    // read the whole file
-	    f.read_to_end(&mut buffer).unwrap();//_or_else( return Err(OmError::Generic( "Error reading font file".to_string() )));
-
-		let collection = FontCollection::from_bytes(&buffer[..] as &[u8]).unwrap_or_else(|e| {
-	        panic!("error constructing a FontCollection from bytes: {}", e);
-    	});
-
-		let font = collection
-		        .into_font() // only succeeds if collection consists of one font
-		        .unwrap_or_else(|e| {
-		            panic!("error turning FontCollection into a Font: {}", e);
-		        });
-		       */
-/*
-		let mut font_file = File::open( &self.fontfile ).unwrap_or_else(|e| {
-			panic!("{}", e);
-		});
-
-		let mut buffer = Vec::new();
-		font_file.read_to_end(&mut buffer).unwrap_or_else(|e| {
-			panic!("{}", e);
-		});
-
-		let font = Font::try_from_vec( buffer ).unwrap(); //.expect( panic!("error constructing a Font from vec" ) );
-
-		self.font = Some( font );
-*/		
 	}
 
 	fn shutdown( &mut self ) {
@@ -136,60 +100,6 @@ let mut f = match File::open(input[ 0 ]) {
 			self.size,					// :TODO: maybe move this to use font
 			self.color
 		);
-/*		
-		if let Some( font ) = &self.font {
-
-			let scale = Scale::uniform( self.size as f32 );
-			let start = point( self.x as f32, ( self.y + self.size ) as f32 );
-			let glyphs: Vec<_> = font.layout( &self.display_text, scale, start).collect();
-//			dbg!(&glyphs);
-
-			for g in glyphs {
-				if let Some( bb ) = &g.pixel_bounding_box() {
-					g.draw(|x, y, v| {
-						if v > 0.0 {
-							let x = ( bb.min.x as u32 + x ) as u32;
-							let y = ( bb.min.y as u32 + y ) as u32;
-
-							let o = ( y * render_buffer.width as u32 + x ) as usize;
-							if o < render_buffer.buffer.len() {
-								let old_pixel = Pixel::from_u32( render_buffer.buffer[ o ] );
-								let new_pixel = Pixel::from_u32( self.color );
-								let pixel = Pixel::blend_with_alpha_and_opacity( new_pixel, old_pixel, v );
-								render_buffer.buffer[ o ] = pixel.to_u32();
-							}
-						}
-					});
-				/*
-					let x = bb.min.x;
-					let y = bb.min.y;
-					let w = bb.max.x - x;
-					let h = bb.max.y - y;
-					TextElement::fill_box( buffer, width, height, x as u32, y as u32, w as u32, h as u32, self.color );
-				*/
-				}
-
-			}
-//			TextElement::fill_box( buffer, width, height, self.x, self.y, self.width, self.height, self.color );
-			/*
-			for y in 0..self.height {
-				let py = y + self.y;
-				if py >= height as u32 { continue; }
-				for x in 0..self.width {
-					let px = x + self.x;
-					if px >= width as u32 { continue; }
-
-	//				dbg!(&px, &py);
-
-					let o = ( py * width as u32 + px ) as usize;
-					buffer[ o ] = self.color;
-				}
-			}
-			*/
-
-		}
-*/
-
 	}
 	fn name( &self ) -> &str {
 		&self.name
