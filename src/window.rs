12819420ad624169975dd4ce06f93ae97ae6fc04
Andreas Neukoetter <andreas@omni-mad.com>
2021-04-27 19:09:23 +0200

    ! Fix build for M1 macOS.

--- src/window.rs
+++ src/window.rs
@@ -10,14 +10,22 @@ impl WindowFactory {
 	pub fn get_default_window_type( ) -> String {
 		"minifb".to_string()
 	}
+	#[cfg(target_arch = "aarch64")]
+	pub fn get_default_window_type( ) -> String {
+		"minifb".to_string()
+	}
 	#[cfg(target_arch = "arm")]
 	pub fn get_default_window_type( ) -> String {
 		"framebuffer".to_string()
 	}
+
+
 	pub fn create( window_type: &str, scaling: f32 ) -> Box< dyn Window >{
 		match window_type {
 			"png" => Box::new( WindowPng::new( scaling ) ),
 	#[cfg(target_arch = "x86_64")]
+			"minifb" => Box::new( WindowMinifb::new() ),
+	#[cfg(target_arch = "aarch64")]
 			"minifb" => Box::new( WindowMinifb::new() ),
 	#[cfg(target_arch = "arm")]
 			"framebuffer" => Box::new( WindowFramebuffer::new() ),
@@ -36,6 +44,11 @@ pub trait Window {
 #[cfg(target_arch = "x86_64")]
 mod window_minifb;
 #[cfg(target_arch = "x86_64")]
+use window_minifb::WindowMinifb;
+
+#[cfg(target_arch = "aarch64")]
+mod window_minifb;
+#[cfg(target_arch = "aarch64")]
 use window_minifb::WindowMinifb;
 
 
