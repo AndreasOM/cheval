ef2b51865affe53902843c93364e7527bba4e3f1
Andreas Neukoetter <andreas@omni-mad.com>
2022-04-14 19:28:09 +0200

    + Implement page switching for Axum based http api.

--- src/http_api_axum.rs
+++ src/http_api_axum.rs
@@ -7,6 +7,7 @@ use std::net::SocketAddr;
 
 use axum::{
 	Extension,
+	extract::Path,
     routing::{get, post},
     http::StatusCode,
     response::IntoResponse,
@@ -48,6 +49,9 @@ impl HttpApiAxum {
 		);
 		let app = Router::new()
 			.route("/page/next", get(goto_next_page))
+			.route("/page/prev", get(goto_prev_page))
+			.route("/page/number/:page_no", get(goto_page_number))
+			.route("/page/name/:page_name", get(goto_page_name))
 			.layer(Extension(http_state))
 		;
 		let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
@@ -72,6 +76,95 @@ async fn goto_next_page(
 	let (sender, receiver) = mpsc::channel();
 	let state = state.lock().unwrap();
 	match state.http_sender.send(Message::GotoNextPage(sender)) {
+		Ok(_) => match receiver.recv() {
+			Ok(msg) => match msg {
+				Response::PageChanged(new_page_no, old_page_no) => {
+					return format!(
+						r#"{{ "new_page":{}, "old_page":{} }}"#,
+						new_page_no.unwrap_or(usize::MAX),
+						old_page_no.unwrap_or(usize::MAX)
+					);
+				},
+				_ => {
+					dbg!(&msg);
+				},
+			},
+			Err(e) => {
+				dbg!(&e);
+			},
+		},
+		_ => {},
+	};
+	"{}".to_string()
+}
+
+async fn goto_prev_page(
+	Extension(state): Extension<Arc<std::sync::Mutex<HttpState>>>,
+) -> impl IntoResponse {
+	debug!("goto_prev_page");
+	let (sender, receiver) = mpsc::channel();
+	let state = state.lock().unwrap();
+	match state.http_sender.send(Message::GotoPrevPage(sender)) {
+		Ok(_) => match receiver.recv() {
+			Ok(msg) => match msg {
+				Response::PageChanged(new_page_no, old_page_no) => {
+					return format!(
+						r#"{{ "new_page":{}, "old_page":{} }}"#,
+						new_page_no.unwrap_or(usize::MAX),
+						old_page_no.unwrap_or(usize::MAX)
+					);
+				},
+				_ => {
+					dbg!(&msg);
+				},
+			},
+			Err(e) => {
+				dbg!(&e);
+			},
+		},
+		_ => {},
+	};
+	"{}".to_string()
+}
+
+async fn goto_page_number(
+	Extension(state): Extension<Arc<std::sync::Mutex<HttpState>>>,
+	Path(page_no): Path<usize>
+) -> impl IntoResponse {
+	debug!("goto_page_number {}", page_no);
+	let (sender, receiver) = mpsc::channel();
+	let state = state.lock().unwrap();
+	match state.http_sender.send(Message::GotoPage(sender, page_no)) {
+		Ok(_) => match receiver.recv() {
+			Ok(msg) => match msg {
+				Response::PageChanged(new_page_no, old_page_no) => {
+					return format!(
+						r#"{{ "new_page":{}, "old_page":{} }}"#,
+						new_page_no.unwrap_or(usize::MAX),
+						old_page_no.unwrap_or(usize::MAX)
+					);
+				},
+				_ => {
+					dbg!(&msg);
+				},
+			},
+			Err(e) => {
+				dbg!(&e);
+			},
+		},
+		_ => {},
+	};
+	"{}".to_string()
+}
+
+async fn goto_page_name(
+	Extension(state): Extension<Arc<std::sync::Mutex<HttpState>>>,
+	Path(page_name): Path<String>
+) -> impl IntoResponse {
+	debug!("goto_page_name {}", page_name);
+	let (sender, receiver) = mpsc::channel();
+	let state = state.lock().unwrap();
+	match state.http_sender.send(Message::GotoPageName(sender, page_name)) {
 		Ok(_) => match receiver.recv() {
 			Ok(msg) => match msg {
 				Response::PageChanged(new_page_no, old_page_no) => {
